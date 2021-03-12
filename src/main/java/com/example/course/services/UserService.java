82ced0ac76163dc6a2f52a78d3bbf829be1054bd
jminango20 <jcarlosminango@gmail.com>
2021-03-12 14:46:07 -0300

    User delete

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -27,4 +27,9 @@ public class UserService {
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+	
+	//324 Delete User
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 }
