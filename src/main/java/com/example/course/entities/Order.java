d4049766523d8d9c0c0666b1021f56214e2b11d0
jminango20 <jcarlosminango@gmail.com>
2021-03-12 11:19:20 -0300

    Payment, one-to-one association

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.example.course.entities.enums.OrderStatus;
@@ -42,6 +44,10 @@ public class Order implements Serializable{
 	//319 One-To-Many
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	//321
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -94,6 +100,16 @@ public class Order implements Serializable{
 		return items;
 	}
 	
+	//321 Get & Set for payment
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -118,6 +134,8 @@ public class Order implements Serializable{
 			return false;
 		return true;
 	}
+
+	
 	
 
 }
